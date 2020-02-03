e8bcfe07f1f4444d66a7e39d2685356b4a06133b
bezigon <bezigon@gmail.com>
2020-02-03 18:11:05 +0200

    Initial Commit

---
+++ GiftsForEveryone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiftsForEveryone
+//
+//  Created by bezigon on 2/3/20.
+//  Copyright © 2020 bezigon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
